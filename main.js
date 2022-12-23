0df3f9b25fab7bb0f8314b087c2098889aa11c02
StacyMch <greensummertea@gmail.com>
2022-12-24 01:43:42 +0300

    добавлено обновление кода накануне 24.12.22 без обновления истории чата каждые 5 с

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
         document.getElementById('menu-right').classList.toggle('menu-right-active');
     }
     
+    //функция отправки сообщений
     function sendMessage() {
 
         //получаем ввод от пользователя
@@ -14,41 +15,15 @@
         xhr.open('GET','https://Messenger.stacymch.repl.co/?messeg=' + msg + '&name=' + nameMy,false);
         xhr.send();
 
-        let json = sendRequestGET("https://Messenger.stacymch.repl.co/index.php");
+        //используем прописанную ниже функцию отрисовки сообщений из jsonа с сервера
+        renderMessages();
 
-        //раскодируем данные
-        let data = JSON.parse(json); 
-        
-        console.log(data);
-
-        for (let i = 0; i < data.length; i++) {
-            
-            if (nameMy == data[i]['name']) {
-
-            //выводим данные шаблона с добавлением класса "справа" для своих сообщений
-            document.getElementById('chat-box__body').innerHTML += document.getElementById('tmpl_mes').innerHTML.replace('${name}', data[i]["name"])
-                                                                                                                .replace('${message}', data[i]['messeg'])
-                                                                                                                .replace('${date}', data[i]["date"])
-                                                                                                                .replace('${style}', 'right');
-            } else {
-                
-                //выводим данные шаблона для чужих сообщений
-                document.getElementById('chat-box__body').innerHTML += document.getElementById('tmpl_mes').innerHTML.replace('${name}', data[i]["name"])
-                                                                                                                .replace('${message}', data[i]['messeg'])
-                                                                                                                .replace('${date}', data[i]["date"]);
-            }
-        }
-
-        //автопрокрутка скроллбара к последнему сообщению
-        chatWindow = document.getElementById('chat-box__body'); 
-        var xH = chatWindow.scrollHeight; 
-        chatWindow.scrollTo(0, xH);
-        
         //очистка textarea после отправки сообщения
         document.getElementById('msg').value = '';
 
     }
 
+    //шаблон функции отправки пустого гет-запроса
     function sendRequestGET(url) {
         let xhr = new XMLHttpRequest();
         xhr.open('GET', url, false);
@@ -58,25 +33,54 @@
         return xhr.responseText;
     }
 
-    
-    //Код jquery для растягивания поля сообщения по высоте контента (только для textarea)
-    $(function() {
-        $('#msg').on('input keyup paste', function() {
-          var $el = $(this),
-              offset = $el.innerHeight() - $el.height();
-      
-          if ($el.innerHeight() < this.scrollHeight) {
-            // Grow the field if scroll height is smaller
-            $el.height(this.scrollHeight - offset);
-          } else {
-            // Shrink the field and then re-set it to the scroll height in case it needs to shrink
-            $el.height(1);
-            $el.height(this.scrollHeight - offset);
-          }
-        });
-      });
+    //функция отрисовки сообщений
+    function renderMessages() {
 
-//Поп-ап Serg
+        //используем шаблон функции гет-запроса с нужным url-ом
+        let json = sendRequestGET("https://Messenger.stacymch.repl.co/index.php");
+
+        //раскодируем данные
+        let data = JSON.parse(json); 
+        
+        //console.log(data);
+
+        //html-контейнер, куда будут отрисованы сообщения
+        let container = document.getElementById('chat-box__body');
+
+        //чистим контейнер, чтобы не было дублирования  
+        container.innerHTML = "";
+
+        for (let i = 0; i < data.length; i++) {
+            
+            if (nameMy == data[i]['name']) {
+
+                //отрисовываем "свои" сообщения справа
+                container.innerHTML += document.getElementById('tmpl_mes').innerHTML.replace('${name}', data[i]["name"])
+                                                                                    .replace('${message}', data[i]['messeg'])
+                                                                                    .replace('${date}', data[i]["date"])
+                                                                                    .replace('${style}', 'right');
+            } else {
+                
+                //отрисовываем "чужие" сообщения слева
+                container.innerHTML += document.getElementById('tmpl_mes').innerHTML.replace('${name}', data[i]["name"])
+                                                                                    .replace('${message}', data[i]['messeg'])
+                                                                                    .replace('${date}', data[i]["date"]);
+            }
+        }
+
+        scrollToZero();
+    
+    }
+
+    //автопрокрутка скроллбара к последнему сообщению
+    function scrollToZero() {
+        chatWindow = document.getElementById('chat-box__body'); 
+        let xH = chatWindow.scrollHeight; 
+        chatWindow.scrollTo(0, xH);
+    }
+
+
+//Попаб Serg
 
     let popupBg = document.querySelector('.popupBg');
     let popup = document.querySelector('.popup');
@@ -94,8 +98,10 @@
     })
 
     closePopup.addEventListener('click', ()=>{
-        popupBg.classList.remove('active');
-        popup.classList.remove('active');
+        if(nameMy) {
+            popupBg.classList.remove('active');
+            popup.classList.remove('active');
+        } 
     })
 
     document.addEventListener('click', (e)=>{
@@ -118,5 +124,44 @@
                     popupBg.classList.add('active');
                     popup.classList.add('active');
                 });
-                }
+        }
 
+    // Serg сообщения по энтеру    
+     document.getElementById('msg').addEventListener('keypress', (e)=> {
+                if (e.key === 'Enter') {
+                    sendMessage();
+                }            
+        });
+
+        //отрисовка истории чата, если юзер ввел имя
+        function updateChatHistory() {
+            if (nameMy) {
+                renderMessages(); 
+            }
+        }
+
+        updateChatHistory();
+    // Serg обновление сообщений
+    /* setInterval(function() { 
+        if (nameMy) {
+            renderMessages(); 
+        }
+    }, 5000); */
+
+
+    //Код jquery для растягивания поля сообщения по высоте контента (только для textarea)
+    $(function() {
+        $('#msg').on('input keyup paste', function() {
+            var $el = $(this),
+                offset = $el.innerHeight() - $el.height();
+        
+            if ($el.innerHeight() < this.scrollHeight) {
+            // Grow the field if scroll height is smaller
+            $el.height(this.scrollHeight - offset);
+            } else {
+            // Shrink the field and then re-set it to the scroll height in case it needs to shrink
+            $el.height(1);
+            $el.height(this.scrollHeight - offset);
+            }
+        });
+        });
